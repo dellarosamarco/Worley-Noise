20922c5b961c5d7f3b7f83fb9a6d575be1459082
dellarosamarco <dellarosamarco00@gmail.com>
2022-04-10 13:10:12 +0200

    Mesh positioning

--- Assets/Scripts/WorleyNoiseMesh.cs
+++ Assets/Scripts/WorleyNoiseMesh.cs
@@ -4,23 +4,35 @@ using System.Linq;
 
 public class WorleyNoiseMesh : MonoBehaviour
 {
+    private static WorleyNoiseMesh instance;
+
     [Header("Components")]
+    public Transform meshTransform;
     public MeshFilter meshFilter;
 
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
 
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Start()
     {
         mesh = new Mesh();
         meshFilter.mesh = mesh;
 
-        GenerateWorleyNoiseMesh(10, 10);
+        GenerateWorleyNoiseMesh(12, 9);
     }
 
     public void GenerateWorleyNoiseMesh(int gridSizeX, int gridSizeZ)
     {
+        float xOffset = -gridSizeX / 2;
+        float yOffset = -4;
+        meshTransform.position = Vector3.zero;
+
         vertices = new Vector3[(gridSizeX + 1) * (gridSizeZ + 1)];
         int index = 0;
 
@@ -28,16 +40,18 @@ public class WorleyNoiseMesh : MonoBehaviour
         {
             for (int z = 0; z <= gridSizeZ; z++)
             {
-                vertices[index] = new Vector3(x, Random.Range(0f,0f), z);
+                vertices[index] = new Vector3(x + xOffset, yOffset + Random.Range(-0.2f,0.2f), z);
                 index++;
             }
         }
+
+        triangles = new int[gridSizeX * gridSizeZ * 6];
 
         for (int z = 0; z < gridSizeZ; z++)
         {
             for (int x = 0; x < gridSizeX; x++)
             {
-                triangles = triangles.Concat(generateSquareMesh(gridSizeX * x + x + z, gridSizeX)).ToArray();
+                triangles = triangles.Concat(generateSquareMesh(x * gridSizeZ + x + z, gridSizeZ)).ToArray();
             }
         }
 
