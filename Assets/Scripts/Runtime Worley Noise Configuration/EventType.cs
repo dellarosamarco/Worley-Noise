62a7ad512da589ed4b028ff78009386155c30e1d
dellarosamarco <dellarosamarco00@gmail.com>
2022-04-16 13:20:49 +0200

    web assembly setup + update runtime configuration

---
+++ Assets/Scripts/Runtime Worley Noise Configuration/EventType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Event
+{
+    NOISE_MULTIPLIER,
+    COLOR_INVERSION,
+    DYNAMIC_CELLULAR,
+    DYNAMIC_COLOR,
+    CELLULAR_SPEED
+}
--- Assets/Scripts/Runtime Worley Noise Configuration/Runtime2DConfiguration.cs
+++ Assets/Scripts/Runtime Worley Noise Configuration/Runtime2DConfiguration.cs
@@ -10,14 +10,39 @@ public class Runtime2DConfiguration : MonoBehaviour
     public RectTransform panelRectTransform;
 
     [Header("2D Configuration Components")]
-    public Slider NoiseMultiplier2D;
+    public Slider noiseMultiplier2D;
+    public Slider cellularSpeed;
+    public Toggle colorInversion;
+    public Toggle dynamicCellular;
+    public Toggle dynamicColor;
 
     private float initialPanelRectTransformHeigh;
 
     private void Start()
     {
         initialPanelRectTransformHeigh = panelRectTransform.sizeDelta.y;
-        NoiseMultiplier2D.onValueChanged.AddListener(delegate { sliderHandler(NoiseMultiplier2D.value); });
+
+        noiseMultiplier2D.onValueChanged.AddListener(delegate { 
+            eventsHandler(noiseMultiplier2D.value, Event.NOISE_MULTIPLIER); 
+        });
+
+        cellularSpeed.onValueChanged.AddListener(delegate {
+            eventsHandler(cellularSpeed.value, Event.CELLULAR_SPEED);
+        });
+
+        colorInversion.onValueChanged.AddListener(delegate { 
+            eventsHandler(colorInversion.isOn, Event.COLOR_INVERSION); 
+        });
+
+        dynamicCellular.isOn = false;
+        dynamicCellular.onValueChanged.AddListener(delegate {
+            eventsHandler(dynamicCellular.isOn, Event.DYNAMIC_CELLULAR);
+        });
+
+        dynamicColor.isOn = false;
+        dynamicColor.onValueChanged.AddListener(delegate {
+            eventsHandler(dynamicColor.isOn, Event.DYNAMIC_COLOR);
+        });
     }
 
     public void onToggle()
@@ -34,9 +59,36 @@ public class Runtime2DConfiguration : MonoBehaviour
 
     //2D CHANGE CONFIGURATION EVENTS
     #region
-    public void sliderHandler(float value)
+    public void eventsHandler(float value, Event _event)
     {
-        WorleyNoiseTexture.instance.noiseMultiplier = value;
+        switch (_event)
+        {
+            case Event.NOISE_MULTIPLIER:
+                WorleyNoiseTexture.instance.noiseMultiplier = value;
+                break;
+            case Event.CELLULAR_SPEED:
+                WorleyNoiseTexture.instance.dynamicChunksSpeed = value;
+                break;
+        }
+
+        WorleyNoiseTexture.instance.cellsIteration();
+    }
+
+    public void eventsHandler(bool value, Event _event)
+    {
+        switch (_event)
+        {
+            case Event.COLOR_INVERSION:
+                WorleyNoiseTexture.instance.colorInversion = value;
+                break;
+            case Event.DYNAMIC_CELLULAR:
+                WorleyNoiseTexture.instance.dynamicChunks = value;
+                break;
+            case Event.DYNAMIC_COLOR:
+                WorleyNoiseTexture.instance.dynamicBaseColor = value;
+                break;
+        }
+
         WorleyNoiseTexture.instance.cellsIteration();
     }
     #endregion
